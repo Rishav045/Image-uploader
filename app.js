78ab5042767aa1037e5b89c90af953d433c32bc4
Rishav045 <82647816+Rishav045@users.noreply.github.com>
2023-06-27 09:36:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const start =  () => {
+const start =  async() => {
   try {
-    // await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI);
 
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
